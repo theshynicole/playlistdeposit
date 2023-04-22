44d8de460baf047e5807a26bcc3513851d6e3b1b
theshynicole <99832261+theshynicole@users.noreply.github.com>
2023-04-21 20:55:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+const express = require('express')
+
+// This file is copied from: https://github.com/thelinmichael/spotify-web-api-node/blob/master/examples/tutorial/00-get-access-token.js
+
+const scopes = [
+    'ugc-image-upload',
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'streaming',
+    'app-remote-control',
+    'user-read-email',
+    'user-read-private',
+    'playlist-read-collaborative',
+    'playlist-modify-public',
+    'playlist-read-private',
+    'playlist-modify-private',
+    'user-library-modify',
+    'user-library-read',
+    'user-top-read',
+    'user-read-playback-position',
+    'user-read-recently-played',
+    'user-follow-read',
+    'user-follow-modify'
+  ];
+  
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId: '00fcf11d408f4922bcb1f3fc00926d88',
+    clientSecret: 'f7142e7af614476ca41b7f82f68d603c',
+    redirectUri: 'https://localhost:3000/callback'
+  });
+  
+  const app = express();
+  
+  app.get('/login', (req, res) => {
+    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+  });
+  
+  app.get('/callback', (req, res) => {
+    const error = req.query.error;
+    const code = req.query.code;
+    const state = req.query.state;
+  
+    if (error) {
+      console.error('Callback Error:', error);
+      res.send(`Callback Error: ${error}`);
+      return;
+    }
+  
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        const access_token = data.body['access_token'];
+        const refresh_token = data.body['refresh_token'];
+        const expires_in = data.body['expires_in'];
+  
+        spotifyApi.setAccessToken(access_token);
+        spotifyApi.setRefreshToken(refresh_token);
+  
+        console.log('access_token:', access_token);
+        console.log('refresh_token:', refresh_token);
+  
+        console.log(
+          `Sucessfully retreived access token. Expires in ${expires_in} s.`
+        );
+        res.send('Success! You can now close the window.');
+  
+        setInterval(async () => {
+          const data = await spotifyApi.refreshAccessToken();
+          const access_token = data.body['access_token'];
+  
+          console.log('The access token has been refreshed!');
+          console.log('access_token:', access_token);
+          spotifyApi.setAccessToken(access_token);
+        }, expires_in / 2 * 1000);
+      })
+      .catch(error => {
+        console.error('Error getting Tokens:', error);
+        res.send(`Error getting Tokens: ${error}`);
+      });
+  });
+  
+  app.listen(8888, () =>
+    console.log(
+      'HTTP Server up. Now go to http://localhost:3000/login in your browser.'
+    )
+  );
+
+
+
+//link to code used:https://github.com/tombaranowicz/SpotifyPlaylistExport/blob/master/index.js
+
